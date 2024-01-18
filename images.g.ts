9c426ec1d0846c6931b94964fc7057c2f50e96ca
NixonBoucher <156244710+NixonBoucher@users.noreply.github.com>
2024-01-18 10:36:49 -0600

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

---
+++ images.g.ts
@@ -0,0 +1,63 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "myImage":return img`
+. . . . c c c c c c . . . . . . 
+. . . c 3 2 2 2 2 3 c . . . . . 
+. . c 2 2 2 2 2 2 2 2 c . . . . 
+. c 3 2 2 2 2 2 2 2 2 3 c . . . 
+. c 2 c 3 3 3 3 c 2 2 2 c . . . 
+. f 2 3 f 3 3 f 3 2 2 2 f . . . 
+. f 2 2 2 2 2 2 2 2 2 2 f . . . 
+. . f 2 2 2 2 3 c 2 2 3 f c . . 
+. . . f c c c c 2 2 3 f 2 2 c . 
+. . c 2 6 2 2 2 3 c f 2 2 2 2 c 
+. c 2 2 6 2 2 c f c 3 2 2 3 c c 
+c 1 1 1 1 2 3 f c c 3 3 3 c . . 
+f 1 1 1 1 1 3 3 c 3 3 3 3 f . . 
+f 3 1 1 1 1 1 3 3 3 3 3 c f . . 
+. f 3 1 1 1 1 1 1 3 3 3 f . . . 
+. . c c c c c c c c c f . . . . 
+`;
+            case "image2":
+            case "myImage0":return img`
+. . . c c c c c c . . . . . . . 
+. . c 3 2 2 2 2 3 c . . . . . . 
+. c 2 2 2 2 2 2 2 2 c . . . . . 
+c 3 2 2 2 2 2 2 2 2 3 c . . . . 
+c 2 c 3 3 3 3 c 2 2 2 c . . . . 
+f 2 3 f 3 3 f 3 2 2 2 f . . . . 
+f 2 2 2 2 2 2 2 2 2 2 f . . . . 
+. f 2 2 2 2 3 c 2 2 3 f . . . . 
+. . f c c c c 2 2 3 f c c c . . 
+. . c 3 6 2 2 2 f c c 2 2 2 c . 
+. c 3 2 2 6 2 2 c f 3 2 2 2 2 c 
+. c 1 1 1 1 2 3 3 c 3 3 3 c c c 
+. c 1 1 1 1 1 3 3 3 3 3 3 c . . 
+. c 3 1 1 1 1 1 3 3 3 3 3 c . . 
+. . c 3 1 1 1 1 1 2 3 3 c c . . 
+. . . c c c c c c c c c c . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
