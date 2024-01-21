982f6b2f1a5f6dfadb30ad44ccbba7e3d7e032a7
CristianGo165 <137358764+CristianGo165@users.noreply.github.com>
2024-01-21 04:05:07 -0600

    Added enemies (3 types). Added enemy behavior. Added Player lives and score. Added Player melee and ranged attacks (Basics). Added Enemy Spawners

--- main.ts
+++ main.ts
@@ -21,16 +21,18 @@ function setUpEnemy (_type: number, posX: number, posY: number) {
     if (_type == 0) {
         enemySprite = sprites.create(assets.image`myImage0`, SpriteKind.Enemy)
         sprites.setDataNumber(enemySprite, "health", 50)
-        sprites.setDataNumber(enemySprite, "speed", 100)
+        sprites.setDataNumber(enemySprite, "speed", 15)
         sprites.setDataNumber(enemySprite, "level", 5)
     } else if (_type == 1) {
         enemySprite = sprites.create(assets.image`myImage`, SpriteKind.Enemy)
         sprites.setDataNumber(enemySprite, "health", 100)
-        sprites.setDataNumber(enemySprite, "speed", 10)
+        sprites.setDataNumber(enemySprite, "speed", 30)
+        sprites.setDataNumber(enemySprite, "level", 15)
     } else if (_type == 2) {
         enemySprite = sprites.create(assets.image`Enemy3`, SpriteKind.Enemy)
         sprites.setDataNumber(enemySprite, "health", 150)
-        sprites.setDataNumber(enemySprite, "speed", 15)
+        sprites.setDataNumber(enemySprite, "speed", 45)
+        sprites.setDataNumber(enemySprite, "level", 20)
     }
     enemySprite.setPosition(posX, posY)
 }
@@ -70,11 +72,103 @@ mp.onButtonEvent(mp.MultiplayerButton.Right, ControllerButtonEvent.Released, fun
     }
     animationHandler(mp.getPlayerSprite(player2), false, currentPlayer, 3, animationSpeed)
 })
+mp.onButtonEvent(mp.MultiplayerButton.A, ControllerButtonEvent.Pressed, function (player2) {
+    if (player2 == mp.playerSelector(mp.PlayerNumber.Two) || player2 == mp.playerSelector(mp.PlayerNumber.Four)) {
+        projectile = sprites.createProjectileFromSprite(assets.image`myImage1`, mp.getPlayerSprite(player2), mp.getPlayerSprite(player2).vx * projectileSpeedConst, mp.getPlayerSprite(player2).vy * projectileSpeedConst)
+    }
+    if (player2 == mp.playerSelector(mp.PlayerNumber.One) || player2 == mp.playerSelector(mp.PlayerNumber.Three)) {
+        for (let value of sprites.allOfKind(SpriteKind.Enemy)) {
+            if (distance(mp.getPlayerSprite(player2), value) < meleeHit) {
+                sprites.destroy(value, effects.spray, 100)
+                mp.changePlayerStateBy(player2, MultiplayerState.score, 100 * mp.getPlayerState(player2, MultiplayerState.life))
+            }
+        }
+    }
+})
 function sprintfunction2 (sprite: Sprite) {
     if (sprintBar2.value > 0) {
         sprintBar2.attachToSprite(sprite, 5, 0)
         sprintBar2.setColor(4, 5)
         statEffect2 = true
+    }
+}
+function initializeSpritesList () {
+    playerSpriteList = [
+    sprites.create(img`
+        . . . . f f f f . . . . . 
+        . . f f f f f f f f . . . 
+        . f f f f f f c f f f . . 
+        f f f f f f c c f f f c . 
+        f f f c f f f f f f f c . 
+        c c c f f f e e f f c c . 
+        f f f f f e e f f c c f . 
+        f f f b f e e f b f f f . 
+        . f 4 1 f 4 4 f 1 4 f . . 
+        . f e 4 4 4 4 4 4 e f . . 
+        . f f f e e e e f f f . . 
+        f e f b 7 7 7 7 b f e f . 
+        e 4 f 7 7 7 7 7 7 f 4 e . 
+        e e f 6 6 6 6 6 6 f e e . 
+        . . . f f f f f f . . . . 
+        . . . f f . . f f . . . . 
+        `, SpriteKind.Player),
+    sprites.create(img`
+        . f f f . f f f f . f f f . 
+        f f f f f c c c c f f f f f 
+        f f f f b c c c c b f f f f 
+        f f f c 3 c c c c 3 c f f f 
+        . f 3 3 c c c c c c 3 3 f . 
+        . f c c c c 4 4 c c c c f . 
+        . f f c c 4 4 4 4 c c f f . 
+        . f f f b f 4 4 f b f f f . 
+        . f f 4 1 f d d f 1 4 f f . 
+        . . f f d d d d d d f f . . 
+        . . e f e 4 4 4 4 e f e . . 
+        . e 4 f b 3 3 3 3 b f 4 e . 
+        . 4 d f 3 3 3 3 3 3 c d 4 . 
+        . 4 4 f 6 6 6 6 6 6 f 4 4 . 
+        . . . . f f f f f f . . . . 
+        . . . . f f . . f f . . . . 
+        `, SpriteKind.Player),
+    sprites.create(img`
+        . . . . f f f f . . . . 
+        . . f f e e e e f f . . 
+        . f f e e e e e e f f . 
+        f f f f 4 e e e f f f f 
+        f f f 4 4 4 e e f f f f 
+        f f f 4 4 4 4 e e f f f 
+        f 4 e 4 4 4 4 4 4 e 4 f 
+        f 4 4 f f 4 4 f f 4 4 f 
+        f e 4 d d d d d d 4 e f 
+        . f e d d b b d d e f . 
+        . f f e 4 4 4 4 e f f . 
+        e 4 f b 1 1 1 1 b f 4 e 
+        4 d f 1 1 1 1 1 1 f d 4 
+        4 4 f 6 6 6 6 6 6 f 4 4 
+        . . . f f f f f f . . . 
+        . . . f f . . f f . . . 
+        `, SpriteKind.Player),
+    sprites.create(img`
+        . . . . . f f f f . . . . . 
+        . . . f f 5 5 5 5 f f . . . 
+        . . f 5 5 5 5 5 5 5 5 f . . 
+        . f 5 5 5 5 5 5 5 5 5 5 f . 
+        . f 5 5 5 d b b d 5 5 5 f . 
+        f 5 5 5 b 4 4 4 4 b 5 5 5 f 
+        f 5 5 c c 4 4 4 4 c c 5 5 f 
+        f b b f b f 4 4 f b f b b f 
+        f b b 4 1 f d d f 1 4 b b f 
+        . f b f d d d d d d f b f . 
+        . f e f e 4 4 4 4 e f e f . 
+        . e 4 f 6 9 9 9 9 6 f 4 e . 
+        . 4 d c 9 9 9 9 9 9 c d 4 . 
+        . 4 f b 3 b 3 b 3 b b f 4 . 
+        . . f f 3 b 3 b 3 3 f f . . 
+        . . . . f f b b f f . . . . 
+        `, SpriteKind.Player)
+    ]
+    for (let value of playerSpriteList) {
+        value.setPosition(-20, -20)
     }
 }
 function sprintfunction (sprite: Sprite) {
@@ -1029,6 +1123,9 @@ function spawnSpawners () {
         setUpEnemySpawners(value.x, value.y)
     }
 }
+function distance (sprite1: Sprite, sprite2: Sprite) {
+    return Math.sqrt(Math.abs(sprite1.x - sprite2.x) ** 2 + Math.abs(sprite1.y - sprite2.y) ** 2)
+}
 mp.onButtonEvent(mp.MultiplayerButton.Left, ControllerButtonEvent.Pressed, function (player2) {
     if (player2 == mp.playerSelector(mp.PlayerNumber.One)) {
         currentPlayer = 0
@@ -1048,6 +1145,9 @@ function sprintfunction3 (sprite: Sprite) {
         statEffect3 = true
     }
 }
+mp.onLifeZero(function (player2) {
+    sprites.destroy(mp.getPlayerSprite(player2), effects.rings, 200)
+})
 mp.onButtonEvent(mp.MultiplayerButton.Down, ControllerButtonEvent.Released, function (player2) {
     if (player2 == mp.playerSelector(mp.PlayerNumber.One)) {
         currentPlayer = 0
@@ -1066,7 +1166,7 @@ function setUpEnemySpawners (spawnerX: number, spawnerY: number) {
     sprites.setDataNumber(enemySpawnerSprite, "radius", randint(0, 25))
     sprites.setDataNumber(enemySpawnerSprite, "angle", randint(0, 360))
     sprites.setDataNumber(enemySpawnerSprite, "spawnEnemyType", randint(0, 2))
-    for (let index = 0; index < 5; index++) {
+    for (let index = 0; index < 3; index++) {
         sprites.setDataNumber(enemySpawnerSprite, "spriteX", enemySpawnerSprite.x + sprites.readDataNumber(enemySpawnerSprite, "radius") * Math.cos(sprites.readDataNumber(enemySpawnerSprite, "angle")))
         sprites.setDataNumber(enemySpawnerSprite, "spriteY", enemySpawnerSprite.y + sprites.readDataNumber(enemySpawnerSprite, "radius") * Math.sin(sprites.readDataNumber(enemySpawnerSprite, "angle")))
         burnerSprite.setPosition(sprites.readDataNumber(enemySpawnerSprite, "spriteX"), sprites.readDataNumber(enemySpawnerSprite, "spriteY"))
@@ -1092,86 +1192,18 @@ mp.onButtonEvent(mp.MultiplayerButton.Up, ControllerButtonEvent.Pressed, functio
 })
 mp.onControllerEvent(ControllerEvent.Connected, function (player2) {
     playerArray = mp.allPlayers()
-    playerSpriteList = [
-    sprites.create(img`
-        . . . . f f f f . . . . . 
-        . . f f f f f f f f . . . 
-        . f f f f f f c f f f . . 
-        f f f f f f c c f f f c . 
-        f f f c f f f f f f f c . 
-        c c c f f f e e f f c c . 
-        f f f f f e e f f c c f . 
-        f f f b f e e f b f f f . 
-        . f 4 1 f 4 4 f 1 4 f . . 
-        . f e 4 4 4 4 4 4 e f . . 
-        . f f f e e e e f f f . . 
-        f e f b 7 7 7 7 b f e f . 
-        e 4 f 7 7 7 7 7 7 f 4 e . 
-        e e f 6 6 6 6 6 6 f e e . 
-        . . . f f f f f f . . . . 
-        . . . f f . . f f . . . . 
-        `, SpriteKind.Player),
-    sprites.create(img`
-        . f f f . f f f f . f f f . 
-        f f f f f c c c c f f f f f 
-        f f f f b c c c c b f f f f 
-        f f f c 3 c c c c 3 c f f f 
-        . f 3 3 c c c c c c 3 3 f . 
-        . f c c c c 4 4 c c c c f . 
-        . f f c c 4 4 4 4 c c f f . 
-        . f f f b f 4 4 f b f f f . 
-        . f f 4 1 f d d f 1 4 f f . 
-        . . f f d d d d d d f f . . 
-        . . e f e 4 4 4 4 e f e . . 
-        . e 4 f b 3 3 3 3 b f 4 e . 
-        . 4 d f 3 3 3 3 3 3 c d 4 . 
-        . 4 4 f 6 6 6 6 6 6 f 4 4 . 
-        . . . . f f f f f f . . . . 
-        . . . . f f . . f f . . . . 
-        `, SpriteKind.Player),
-    sprites.create(img`
-        . . . . f f f f . . . . 
-        . . f f e e e e f f . . 
-        . f f e e e e e e f f . 
-        f f f f 4 e e e f f f f 
-        f f f 4 4 4 e e f f f f 
-        f f f 4 4 4 4 e e f f f 
-        f 4 e 4 4 4 4 4 4 e 4 f 
-        f 4 4 f f 4 4 f f 4 4 f 
-        f e 4 d d d d d d 4 e f 
-        . f e d d b b d d e f . 
-        . f f e 4 4 4 4 e f f . 
-        e 4 f b 1 1 1 1 b f 4 e 
-        4 d f 1 1 1 1 1 1 f d 4 
-        4 4 f 6 6 6 6 6 6 f 4 4 
-        . . . f f f f f f . . . 
-        . . . f f . . f f . . . 
-        `, SpriteKind.Player),
-    sprites.create(img`
-        . . . . . f f f f . . . . . 
-        . . . f f 5 5 5 5 f f . . . 
-        . . f 5 5 5 5 5 5 5 5 f . . 
-        . f 5 5 5 5 5 5 5 5 5 5 f . 
-        . f 5 5 5 d b b d 5 5 5 f . 
-        f 5 5 5 b 4 4 4 4 b 5 5 5 f 
-        f 5 5 c c 4 4 4 4 c c 5 5 f 
-        f b b f b f 4 4 f b f b b f 
-        f b b 4 1 f d d f 1 4 b b f 
-        . f b f d d d d d d f b f . 
-        . f e f e 4 4 4 4 e f e f . 
-        . e 4 f 6 9 9 9 9 6 f 4 e . 
-        . 4 d c 9 9 9 9 9 9 c d 4 . 
-        . 4 f b 3 b 3 b 3 b b f 4 . 
-        . . f f 3 b 3 b 3 3 f f . . 
-        . . . . f f b b f f . . . . 
-        `, SpriteKind.Player)
-    ]
     mp.setPlayerSprite(player2, playerSpriteList[spriteIndex])
     mp.getPlayerSprite(player2).setPosition(250, 900)
     spriteIndex += 1
     scene.cameraFollowSprite(mp.getPlayerSprite(player2))
+    mp.setPlayerState(player2, MultiplayerState.life, 3)
+    mp.setPlayerState(mp.playerSelector(mp.PlayerNumber.One), MultiplayerState.score, 0)
 })
 function initializeVariables () {
+    canHurtPlayers = true
+    projectileSpeedConst = 5
+    meleeHit = 30
+    enemySight = 100
     burnerSprite = sprites.create(img`
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
@@ -1334,12 +1366,28 @@ function initializeVariables () {
     scene.setBackgroundColor(15)
     tiles.setCurrentTilemap(tilemap`Overworld`)
 }
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    sprites.destroy(sprite)
+    sprites.destroy(otherSprite, effects.trail, 100)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    if (canHurtPlayers) {
+        scene.cameraShake(6, 500)
+        mp.changePlayerStateBy(mp.getPlayerBySprite(sprite), MultiplayerState.life, -1)
+        canHurtPlayers = false
+        sprite.startEffect(effects.spray, 100)
+        timer.after(5000, function () {
+            canHurtPlayers = true
+        })
+    }
+})
 let speed4 = 0
 let speed3 = 0
 let speed2 = 0
 let speed = 0
+let enemySight = 0
+let canHurtPlayers = false
 let spriteIndex = 0
-let playerSpriteList: Sprite[] = []
 let playerArray: mp.Player[] = []
 let burnerSprite: Sprite = null
 let enemySpawnerSprite: Sprite = null
@@ -1350,11 +1398,16 @@ let sprintBar4: StatusBarSprite = null
 let animList: Image[][][] = []
 let statEffect = false
 let sprintBar: StatusBarSprite = null
+let playerSpriteList: Sprite[] = []
 let statEffect2 = false
 let sprintBar2: StatusBarSprite = null
+let meleeHit = 0
+let projectileSpeedConst = 0
+let projectile: Sprite = null
 let enemySprite: Sprite = null
 let animationSpeed = 0
 let currentPlayer = 0
+initializeSpritesList()
 namespace userconfig {
     export const ARCADE_SCREEN_WIDTH = 320
     export const ARCADE_SCREEN_HEIGHT = 240
@@ -1363,6 +1416,15 @@ initializeVariables()
 animSetup()
 spawnSpawners()
 game.splash("Town Game")
+game.onUpdateInterval(1000, function () {
+    for (let value of sprites.allOfKind(SpriteKind.Enemy)) {
+        if (distance(mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.One)), value) < enemySight) {
+            value.follow(mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.One)), sprites.readDataNumber(value, "speed"))
+        } else {
+            value.setVelocity(randint(-1 * sprites.readDataNumber(value, "speed"), sprites.readDataNumber(value, "speed")), randint(-1 * sprites.readDataNumber(value, "speed"), sprites.readDataNumber(value, "speed")))
+        }
+    }
+})
 forever(function () {
     mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.One), speed, speed)
     if (mp.isButtonPressed(mp.playerSelector(mp.PlayerNumber.One), mp.MultiplayerButton.B) && sprintBar.value > 0) {
@@ -1387,29 +1449,6 @@ forever(function () {
     }
 })
 forever(function () {
-    mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.Two), speed2, speed2)
-    if (mp.isButtonPressed(mp.playerSelector(mp.PlayerNumber.Two), mp.MultiplayerButton.B) && sprintBar2.value > 0) {
-        sprintfunction2(mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two)))
-        pause(100)
-        sprintBar2.setStatusBarFlag(StatusBarFlag.SmoothTransition, true)
-        sprintBar2.value += -5
-        speed2 += 10
-        animationSpeed = 100
-    } else {
-        speed2 = 50
-        animationSpeed = 200
-    }
-    if (!(mp.isButtonPressed(mp.playerSelector(mp.PlayerNumber.Two), mp.MultiplayerButton.B)) && sprintBar2.value < 100) {
-        pause(300)
-        sprintBar2.value += 5
-        if (statEffect2) {
-            if (sprintBar2.value == 100) {
-                sprintBar2.setColor(3, 5)
-            }
-        }
-    }
-})
-forever(function () {
     mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.Three), speed3, speed3)
     if (mp.isButtonPressed(mp.playerSelector(mp.PlayerNumber.Three), mp.MultiplayerButton.B) && sprintBar3.value > 0) {
         sprintfunction3(mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Three)))
@@ -1428,6 +1467,29 @@ forever(function () {
         if (statEffect3) {
             if (sprintBar3.value == 100) {
                 sprintBar3.setColor(3, 5)
+            }
+        }
+    }
+})
+forever(function () {
+    mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.Two), speed2, speed2)
+    if (mp.isButtonPressed(mp.playerSelector(mp.PlayerNumber.Two), mp.MultiplayerButton.B) && sprintBar2.value > 0) {
+        sprintfunction2(mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two)))
+        pause(100)
+        sprintBar2.setStatusBarFlag(StatusBarFlag.SmoothTransition, true)
+        sprintBar2.value += -5
+        speed2 += 10
+        animationSpeed = 100
+    } else {
+        speed2 = 50
+        animationSpeed = 200
+    }
+    if (!(mp.isButtonPressed(mp.playerSelector(mp.PlayerNumber.Two), mp.MultiplayerButton.B)) && sprintBar2.value < 100) {
+        pause(300)
+        sprintBar2.value += 5
+        if (statEffect2) {
+            if (sprintBar2.value == 100) {
+                sprintBar2.setColor(3, 5)
             }
         }
     }
